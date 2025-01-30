b47cd8e000d004995efbeb66538f6dfd4fa627f5
LouisPino <ljpino95@gmail.com>
2025-01-29 20:27:34 -0500

    test

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,6 +5,9 @@ console.log("YOU HAVE HIT ME")
 
 mongoose.set('strictQuery', true);
 mongoose.connect(DATABASE_URI)
+    .then(() => console.log("You are connected to mongoose"))
+    .catch((error) => console.log("Error connecting to mongoose:", error));
+
 
 // Connection Events
 mongoose.connection
