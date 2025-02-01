88849a7cdf56940d47adbcede95b16ac95fa5ce7
LouisPino <ljpino95@gmail.com>
2025-01-31 19:43:27 -0500

    delete prompt

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,7 +8,21 @@ import { GoogleLogin, GoogleOAuthProvider, useGoogleLogin } from '@react-oauth/g
 import("../styles/header.css")
 
 export default function Header({ user, setUser }) {
+    const [inAppBrowser, setinAppBrowser] = useState(false)
     const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID
+    function isInAppBrowser() {
+        const ua = navigator.userAgent || navigator.vendor || window.opera;
+        return /Instagram|FBAN|FBAV/i.test(ua);
+    }
+
+    useEffect(() => {
+        if (isInAppBrowser()) {
+            setinAppBrowser(true)
+            alert("For the best experience, please open this link in your default browser.");
+            // Optionally, you can provide a button or link to assist users
+            // in opening the page in their default browser.
+        }
+    }, []);
 
     return (
         <div className="header ">
@@ -18,6 +32,10 @@ export default function Header({ user, setUser }) {
                 <Link className="header-link" to="/artists"><p className="link-text">Artists</p></Link>
                 <Link className="header-link" id="random-header" to="/random"><p className="link-text">Random</p></Link>
             </div>
+            {inAppBrowser && <div id="open-in-browser" className="you-sure">
+                <p>For the best experience, please open this page in your default browser.</p>
+                <a href="/" target="_blank" rel="noopener noreferrer">Open in Browser</a>
+            </div>}
             {user ?
                 <div className="header-r">
                     <Link className="header-link" to="/albums/add"><p className="link-text">Add Music</p></Link>
