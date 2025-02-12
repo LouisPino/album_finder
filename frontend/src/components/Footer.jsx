6a4aac41a4f8d6dba6e50eae7293d25106df8c99
LouisPino <ljpino95@gmail.com>
2025-02-12 17:16:58 -0500

    donate

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
                 If you have any questions or ideas on how to improve the site please <a href="https://www.instagram.com/ljpino95/" target="_blank">get in touch!</a>
             </p>
             <p>
-                If you would like to help offest hosting costs consider <a href="https://www.instagram.com/ljpino95/" target="_blank">donating! {"<3"}</a>
+                If you would like to help offest hosting costs consider <a href="https://www.paypal.com/donate/?business=SGHR4W67JPFYA&no_recurring=0&item_name=This+site+costs+%24150+per+year+to+host%2C+help+contribute+to+its+longevity+fund%21&currency_code=CAD" target="_blank">donating! {"<3"}</a>
             </p>
         </div>
     )
