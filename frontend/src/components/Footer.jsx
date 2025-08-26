67a0231f1e8039c65fff105d9fe7f27e97fa81ff
LouisPino <ljpino95@gmail.com>
2025-08-26 17:43:49 -0400

    remove paypal

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,9 +7,9 @@ export default function Footer() {
             <p>
                 If you have any questions or ideas on how to improve the site please <a href="https://www.instagram.com/ljpino95/" target="_blank">get in touch!</a>
             </p>
-            <p>
+            {/* <p>
                 If you would like to help offest hosting costs consider <a href="https://www.paypal.com/donate/?business=SGHR4W67JPFYA&no_recurring=0&item_name=This+site+costs+%24150+per+year+to+host%2C+help+contribute+to+its+longevity+fund%21&currency_code=CAD" target="_blank">donating! {"<3"}</a>
-            </p>
+            </p> */}
         </div>
     )
 
